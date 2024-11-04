2d470344623ef4e926f9f2f7edbb370cb1f2b39d
zarkonow <stojanov1987@gmail.com>
2024-11-04 19:44:15 +0100

    *added css

--- src/custom-model-popup/modal-test.jsx
+++ src/custom-model-popup/modal-test.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import Modal from './modal.jsx'
+import './modal.css'
 
 
 
@@ -12,11 +13,19 @@ function handleTogglePopup(){
   setshowModalPopup(!showModalPopup)
 }
 
+function onClose() {
+  setshowModalPopup(false) 
+}
+
   return (
     <div>
       <button onClick={handleTogglePopup}>Open Modal Popup</button>
       {
-        showModalPopup&& <Modal/>
+        showModalPopup && <Modal
+        id={'custom-id'}
+        header={<p>Customized Header</p>}
+        footer={<p>Customized Footer</p>}
+        onClose={onClose} body={<div>Costumized Body</div>}/>
       }
     </div>
   )
--- src/custom-model-popup/modal.jsx
+++ src/custom-model-popup/modal.jsx
@@ -1,9 +1,22 @@
-
-
-export default function Modal() {
+export default function Modal({ id, header, body, footer, onClose }) {
   return (
-    <div>
-        
+    <div id={id || "Modal"} className="modal">
+      <div className="modal-content">
+        <div className="header">
+          <span onClick={onClose} className="close-modal-icon">&times;</span>
+          <h2>{header ? header : "Header"}</h2>
+        </div>
+        <div className="body">
+          {body ? (
+            body
+          ) : (
+            <div>
+              <p>This is our Modal Body</p>
+            </div>
+          )}
+        </div>
+        <div className="footer">{footer ? footer : <h3>Footer</h3>}</div>
+      </div>
     </div>
-  )
+  );
 }
